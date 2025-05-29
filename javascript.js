13a985cc5ac5527629fa75af78af9b501052f365
onewillow <210259229+onewillowbrook@users.noreply.github.com>
2025-05-29 11:00:07 -0400

    Sizing working and boxes no longer overflowing

--- javascript.js
+++ javascript.js
@@ -1,7 +1,6 @@
 const container = document.querySelector('.container');
 const buttonSizer = document.querySelector('.sizer');
 const inputSizer = document.querySelector('.inputSizer');
-const container = document.querySelector('.container');
 
 function createGrid(gridSize) {
     container.innerHTML = '';
@@ -12,40 +11,28 @@ function createGrid(gridSize) {
         box.style.width = `${boxSize}px`;
         box.style.height = `${boxSize}px`;
         box.style.border = '1px solid #ccc';
-        container.appendChild(box);
-    }
-}
-
-
-
-for (let i = 0; i < 16; i++) {
-    const newDiv = document.createElement('div');
-
-    newDiv.classList.add('box');
-
-    newDiv.textContent = `Box ${i + 1}`;
-    container.appendChild(newDiv);
-
-    newDiv.addEventListener('mouseenter', () => {
-        newDiv.style.backgroundColor = 'dodgerblue';
-    });
+        
+        // box.textContent = `Box ${i + 1}`;
+        box.addEventListener('mouseenter', () => {
+            box.style.backgroundColor = 'dodgerblue';
+        });
+        
+        box.addEventListener('mouseleave', () => {
+            box.style.backgroundColor = '';
+        });
     
-    newDiv.addEventListener('mouseleave', () => {
-        newDiv.style.backgroundColor = '';
-    });
+        container.appendChild(box);
+        }
 }
 
-function boxSizer(size) {
-    document.querySelectorAll('.box').forEach(box => {
-        box.style.width = `${size}px`;
-        box.style.height = `${size}px`;
-    })
-};
+
+
+createGrid(16);
 
 buttonSizer.addEventListener("click", () => {
     const size = parseInt(inputSizer.value);
     if (!isNaN(size) && size > 9 && size < 101) {
-        boxSizer(size);
+        createGrid(size);
     } else {
         alert("Please enter a valid size: 10 <-> 100 are allowed.");
     }
